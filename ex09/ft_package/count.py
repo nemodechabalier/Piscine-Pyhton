dbaeabee5dd453c3e4005abb7500d4310515ded9
Nemo De Chabalier <nde-chab@f2r3s12.clusters.42paris.fr>
2026-01-15 17:06:22 +0100

    ex 09

---
+++ ex09/ft_package/count.py
@@ -0,0 +1,12 @@
+def count_in_list(lst: list, value) -> int:
+    """
+    Count how many times 'value' appears in the list 'lst'.
+
+    Parameters:
+    - lst: list of elements
+    - value: element to count
+
+    Returns:
+    - int: number of occurrences of value in lst
+    """
+    return lst.count(value)
